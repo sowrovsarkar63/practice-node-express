ecbc1e59d5d1e2e7823eca4679b217e0ed5e096e
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-22 13:11:08 +0600

    daynamic api and search query

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send(
+        "WOW. I am Excited to learn Node and express with nodemon. automatic restart"
+    );
+});
+
+// user array
+
+const users = [
+    {
+        id: 0,
+        name: "Shabana",
+        email: "Shabana@gmail.com",
+        phone: "01788888888",
+    },
+    {
+        id: 1,
+        name: "Shabnoor",
+        email: "Shabnoor@gmail.com",
+        phone: "01788888888",
+    },
+    {
+        id: 2,
+        name: "Shrabonti",
+        email: "Shrabonti@gmail.com",
+        phone: "01788888888",
+    },
+    {
+        id: 3,
+        name: "Suchorita",
+        email: "Suchorita@gmail.com",
+        phone: "01788888888",
+    },
+    { id: 4, name: "Soniya", email: "Soniya@gmail.com", phone: "01788888888" },
+    {
+        id: 5,
+        name: "Susmita",
+        email: "Susmita@gmail.com",
+        phone: "01788888888",
+    },
+];
+
+// daynamic api
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const user = users[id];
+
+    res.send(user);
+});
+
+app.get("/users", (req, res) => {
+    const search = req.query.search;
+
+    // find and display search result
+
+    if (search) {
+        const searchResult = users.filter((user) =>
+            user.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        );
+        res.send(searchResult);
+    } else {
+        res.send(users);
+    }
+});
+
+// Listen app via port
+
+app.listen(port, () => {
+    console.log(`App listening at port ${port}`);
+});
